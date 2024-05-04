f893a051ffcacdfd5d00392a090625573459ab64
HallamSaunders <hals4072@gmail.com>
2024-05-04 20:08:17 +0100

    Created new Svelte app and defined a home route.

---
+++ portfolioapp/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<style>
+    h1 {
+        color: whitesmoke;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: large;
+    }
+
+    p {
+        color: whitesmoke;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: large;
+    }
+</style>
+
+<script>
+    function commandListener() {
+
+    }
+
+    function commandExecuter() {
+        while (true) {
+
+        }
+    }
+</script>
+
+<main>
+    <div>
+        <h1>Welcome to my portfolio.</h1>
+        <p>Type the command "help" to get started.</p>
+    </div>
+</main>
---
+++ portfolioapp/src/routes/tests/+page.svelte
@@ -0,0 +1,56 @@
+<style>
+    h1 {
+        color: whitesmoke;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: large;
+    }
+
+    p {
+        color: whitesmoke;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: large;
+    }
+</style>
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let typedChars = "";
+    let index = 0;
+    let typewriter: any;
+    let isTyping = false;
+
+    const typeChar = (text: string) => {
+        if (index < text.length) {
+            typedChars += text[index];
+            index += 1;
+        } else {
+            stopTyping();
+            return;
+        }
+    }
+
+    const startTyping = (text: string) => {
+        if (!isTyping) {
+            isTyping = true;
+            typewriter = setInterval(() => typeChar(text), 35);
+        }
+    }
+
+    const stopTyping = () => {
+        clearInterval(typewriter);
+        isTyping = false;
+    }
+
+    onMount(() => {
+        //start typing when the component is mounted
+        startTyping("Welcome to my portfolio...");
+    });
+</script>
+
+<main>
+    <div class="header">
+        <h1>{typedChars}</h1>
+        <p>{typedChars}</p>
+    </div>
+</main>
